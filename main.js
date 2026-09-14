f4e76d0751019159e4b6626737d7ce3874944a83
btssam <btsamara@gmail.com>
2026-09-13 20:53:27 -0400

    adjusted square tablet query, as it was triggering on ipad

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ const SVG_NS = 'http://www.w3.org/2000/svg';
 const compactLayoutQuery = window.matchMedia(
     "(max-width: 600px) and (orientation: portrait)," +
     "(min-width: 601px) and (max-width: 1450px) and (orientation: portrait)," +
-    "(max-width: 900px) and (min-aspect-ratio: 4/5) and (max-aspect-ratio: 5/4)," +
+    "(max-width: 900px) and (max-height: 850px) and (min-aspect-ratio: 4/5) and (max-aspect-ratio: 5/4)," +
     "(max-width: 1699px) and (max-height: 500px) and (orientation: landscape)," +
     "(min-width: 601px) and (max-width: 999px) and (orientation: landscape) and (min-height: 501px)," +
     "(min-width: 1000px) and (max-width: 1366px) and (orientation: landscape) and (min-height: 501px) and (pointer: coarse)"
